8d958240fb8d82eb178b219e5cc3d4d1bd7dcb22
JD2929 <jandunk@creonova.com>
2023-09-01 19:24:55 -0300

    License feature added.

---
+++ Develop/README.md
@@ -0,0 +1,43 @@
+# Hello there
+
+  <img src="https://img.shields.io/badge/license-ISC-blue.svg">
+
+
+  ## Description 
+    
+  saying hello to the world	 
+
+  ## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributions](#contributions)
+* [Testing](#testing)
+* [Questions](#questions)
+  
+  ## Installation  
+  
+  there are none
+  
+  ## Usage
+  
+  Hello world 
+  
+## License
+    
+    
+    This project is licensed under ISC.
+
+  ## Contributions 
+  
+  undefined 
+  
+  ## Testing
+  
+  wave hello
+
+  ## Questions
+
+  If you would like to reach out or have any questions please email at [jandunk@creonova.com](mailto:jandunk@creonova.com).
+
+  If you would like to see more of my projects please check out my GiHub at [JD2929](https://github.com/JD2929)
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,79 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `<img src="https://img.shields.io/badge/license-${license}-blue.svg">`
+
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `* [License](#license)`
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License
+    
+    
+    This project is licensed under ${license}.`
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+
+  ## Description 
+    
+  ${data.description} 
+
+  ## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contributions](#contributions)
+* [Testing](#testing)
+* [Questions](#questions)
+  
+  ## Installation  
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage} 
+  
+${renderLicenseSection(data.license)}
+
+  ## Contributions 
+  
+  ${data.contribution} 
+  
+  ## Testing
+  
+  ${data.testing}
+
+  ## Questions
+
+  If you would like to reach out or have any questions please email at [${data.email}](mailto:${data.email}).
+
+  If you would like to see more of my projects please check out my GiHub at [${data.github}](https://github.com/${data.github})
 `;
 }
 
